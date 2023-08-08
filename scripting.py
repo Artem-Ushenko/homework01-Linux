6c2fa6f11ffaa91364bb8235a042b22a81daaf6e
Artem Ushenko <you@example.com>
2023-08-08 16:38:44 +0000

    Updated check requirements function

--- scripting.py
+++ scripting.py
@@ -14,30 +14,33 @@ def install_mysql():
     pass
 
 def check_rquirements():
-   #check Python version 
-    version_info = os.popen('python3 --version 2>&1').read()
-    if 'Python 3.7.2' not in version_info:
-        print("Python 3.7.2 will be installed now: ")
-        install_python()
-    else:
-        print("Python 3.7.2 is already installed.")
+    #check linux disributor id
+    version_info = os.popen('lsb_release -a 2>&1').read()
+    if 'Ubuntu' or 'Debian' in version_info:
+        #check Python version 
+        version_info = os.popen('python3 --version 2>&1').read()
+        if 'Python 3.7.2' not in version_info:
+            print("Python 3.7.2 will be installed now: ")
+            install_python()
+        else:
+            print("Python 3.7.2 is already installed.")
 
-    #check PIP install
-    version_info = os.system('pip --version')
-    if version_info not 0:
-        print("Pip will be installed now: ")
-        install_pip()
-    else:
-        print("Pip is already installed")
+        #check PIP install
+        version_info = os.system('pip --version')
+        if version_info not 0:
+            print("Pip will be installed now: ")
+            install_pip()
+        else:
+            print("Pip is already installed")
 
-    #check MySql version
-    version_info = os.popen('mysql --version 2>&1').read()
-    if 'Mysql-8.0.15' not in version_info:
-        print("Mysql-8.0.15 will be installed now: ")
-        install_mysql()
+        #check MySql version
+        version_info = os.popen('mysql --version 2>&1').read()
+        if 'Mysql-8.0.15' not in version_info:
+            print("Mysql-8.0.15 will be installed now: ")
+            install_mysql()
+        else:
+            print("Mysql-8.0.15 is already installed.")
     else:
-        print("Mysql-8.0.15 is already installed.")
-            
-                                                               
+        print("Please install ans use Linux Ubuntu/Debian distribution")
 
 check_requirements()                                             
