718c28a430984556c90571585de8f52dbb079325
Artem Ushenko <you@example.com>
2023-08-11 23:23:02 +0300

    Updated version of script

--- scripting.py
+++ scripting.py
@@ -4,77 +4,71 @@ import os
 import subprocess
 import sys
 
-def virtual_environment():
-    pass
-
 def install_environment():
     try:
-        subprocess.run(['sudo', 'pip3', 'install', 'virtualenv'])
-        os.mkdir('envs')
+        subprocess.run(['sudo', 'pip', 'install', 'virtualenv'])
+        subprocess.run(["mkdir", "envs"])
         subprocess.run(['virtualenv', './envs/'])
-        subprocess.run(["source", "bin/activate"], shell=True)
+        subprocess.run(["source", "envs/bin/activate"], shell=True)
         git_path = "https://github.com/Manisha-Bayya/simple-django-project.git"
         subprocess.run(['git', 'clone', git_path])
         os.chdir('simple-django-project')
         venv_path = "../envs/bin/"
-        subprocess.run([venv_path + "pip3", "install", "-r", "requirements.txt"])
+        subprocess.run([venv_path + "pip", "install", "-r", "requirements.txt"])
         print("Virtual environment installed successfully.")
     except Exception as e:
         print("Something gone wrong.")
         print(str(e))
 
 def install_python():
-    if sys.version_info.major < 3:
-        print("Your Python version {sys.version_info.major} does not meet the requirements. Please install Python 3.7.2 or higher")
-    elif sys.version_info.minor < 7:
-        print("Your Python version {sys.version_info.major} + '.' + {sys.version_info.minor} does not meet the requirements. Please install Python 3.7.2 or higher")
-    else:
-        version_info = subprocess.run(['python3','--version'], stdout=subprocess.PIPE, universal_newlines=True)
-        print(version_info.stdout + 'already installed and meet the requirements')
-
+    pass
+   
 def install_pip():
-    result = subprocess.run(['sudo', 'apt', 'install', 'python3-pip'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    if result.returncode == 0:
-        return (print("Pip was installed successfully."))
-    else:
-        return (print(f"Pip install failed with error code: {result.returncode}"))
+    try:
+        subprocess.run(['sudo', 'apt', 'update'])
+        subprocess.run(['sudo', 'apt', 'install', 'python3-pip'])
+        print("Pip was installed successfully.")
+    except Exception as e:
+        print(f"Pip install failed...")
+        print(str(e))
 
 def install_mysql():
-    version_info = subprocess.run(['mysql', '--version'], capture_output=True, text=True)
-    if 'mysql  Ver 8' in version_info.stdout:
-        print(f"{version_info.stdout} already installed and meet the requirements")
-    else:
-        print(f"{version_info.stdout} does not meet the requirements. Please install Mysql 8 or higher")
+    try:
+        subprocess.run(['sudo', 'apt', 'update'])
+        subprocess.run(['sudo', 'apt', 'install', 'mysql-server'])
+        version_info = subprocess.run(['mysql', '--version'], capture_output=True)
+        print(f"{version_info.stdout[:17]} was installed successfully.")
+    except Exception as e:
+        print(f"Pip install failed.")
+        print(str(e))
 
 def check_requirements():
-    #check linux disributor id
-    version_info = os.popen('lsb_release -a 2>&1').read()
-    if 'Ubuntu' or 'Debian' in version_info:
-        #check Python version 
-        version_info = os.popen('python3 --version 2>&1').read()
-        if 'Python 3.7.2' not in version_info:
-            print("Python 3.7.2 will be installed now: ")
-            install_python()
-        else:
-            print("Python 3.7.2 is already installed.")
+    #check Python version 
+    if sys.version_info.major < 3:
+        print(f"Your Python version {sys.version_info.major} does not meet the requirements. Please install Python 3.7.2 or higher")
+    if sys.version_info.minor < 7:
+        print(f"Your Python version {sys.version_info.major} + '.' + {sys.version_info.minor} does not meet the requirements. Please install Python 3.7.2 or higher")
+    if sys.version_info.minor >= 7:
+        version_info = subprocess.run(['python3', '--version'], capture_output=True)
+        print(str(version_info.stdout) + 'already installed and meet the requirements')
 
-        #check PIP install
-        version_info = subprocess.run(['pip3',  '--version'])
-        if version_info != 0:
-            print("Pip will be installed now: ")
-            install_pip()
-        else:
-            print("Pip is already installed")
+    #check PIP install
+    try:
+        version_info = subprocess.run(["pip", "--version"])
+        print("Pip is already installed on your system.")
+    except:
+        print("Pip was not found on your system. It will be install now: ")
+        install_pip()
 
-        #check MySql version
-        version_info = subprocess.run(['mysql', '--version', '2>&1'], stdout=subprocess.PIPE, text=True)
-        if 'Mysql-8.0.15' not in version_info.stdout:
-            print("Mysql-8.0.15 will be installed now: ")
+    #check MySql version
+    try:
+        version_info = subprocess.run(['mysql', '--version'], capture_output=True)
+        if 'mysql  Ver 8' not in version_info.stdout:
+            print("Mysql 8 will be installed now: ")
             install_mysql()
-        else:
-            print("Mysql-8.0.15 is already installed.")
-    else:
-        print("Please install ans use Linux Ubuntu/Debian distribution")
+    except:
+        print("Mysql was not found on your system. It will be installed now")
+        install_mysql()
 
 check_requirements()
-virtual_environment()
+install_environment()
