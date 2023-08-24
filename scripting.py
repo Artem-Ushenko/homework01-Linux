7561b22165000d31926b64e1c3452e33aa747eb5
Artem Ushenko <you@example.com>
2023-08-24 14:30:35 +0300

    Updated scripting.py

--- scripting.py
+++ scripting.py
@@ -7,11 +7,37 @@ import re
 import json
 
 
+def check_mysql_permissions(mysql_user):
+    permission_to_mysql = subprocess.run(["mysql", "-u", f"{mysql_user} -p"], capture_output=True, shell=True)
+    if permission_to_mysql.stderr == 0:
+        print(f"Your user {mysql_user} have access to Mysql.")
+    else:
+        print(f"Access denied to Mysql for {mysql_user}.")
 
-def load_data_mysql(mysql_user):
-    subprocess.run(["sudo", "mysql", "-u", mysql_user, "-p", "<", "simple-django-project/world.sql"], shell=True)
+    pass
+def run_server():
+    subprocess.run(["python simple-django-project/manage.py makemigrations"], shell=True)
+    subprocess.run(["python simple-django-project/manage.py migrate"], shell=True)
+    subprocess.run(["python simple-django-project/manage.py rebuild_index"], shell=True)
+    subprocess.run(["python simple-django-project/manage.py runserver 0:8001"], shell=True)
+    print("Your server is up on port 8001")
 
-def modify_settings(mysql_user, mysql_password, mysql_host, mysql_port, email_host, email_host_password):
+#def load_data_mysql():
+    #subprocess.run(["sudo", "mysql_secure_installation"])
+   
+    #subprocess.run(["mysql -u root -p"], shell=True)
+    #mysql_user = 'bob'
+    #mysql_password = 'P@ssword123'
+    #mysql_queries = [
+    #        f"CREATE USER '{mysql_user}'@'localhost' IDENTIFIED BY '{mysql_password}';",
+    #        f"GRANT ALL PRIVILEGES ON world.* TO '{mysql_user}'@'localhost';",
+    #        f"FLUSH PRIVILEGES;"
+    #        ]
+    
+    #for command in mysql_queries:
+    #    subprocess.run([f'mysql -u root -p -e {command}'], shell=True) 
+    
+def modify_settings(mysql_user, mysql_password, mysql_host, mysql_port, email_host_user, email_host_password):
 
     # Temporarily add the directory containing settings.py to sys.path
     sys.path.append('simple-django-project/panorbit/')
@@ -26,7 +52,7 @@ def modify_settings(mysql_user, mysql_password, mysql_host, mysql_port, email_ho
     settings.DATABASES['default']['PASSWORD'] = mysql_password
     settings.DATABASES['default']['HOST'] = mysql_host
     settings.DATABASES['default']['PORT'] = mysql_port
-    settings.EMAIL_HOST = email_host
+    settings.EMAIL_HOST_USER = email_host_user
     settings.EMAIL_HOST_PASSWORD = email_host_password
 
     def replace_database_config(file_path, db_data, email_host, email_host_password):
@@ -58,7 +84,7 @@ def modify_settings(mysql_user, mysql_password, mysql_host, mysql_port, email_ho
 
 def install_environment():
     try:
-        subprocess.run(["python3", "-m", "venv", "myvenv"])
+        subprocess.run(["python", "-m", "venv", "new_venv"])
         git_path = "https://github.com/Manisha-Bayya/simple-django-project.git"
         subprocess.run(["git", "clone", git_path])
         status = subprocess.run(["pip", "install", "-r", "requirements.txt"], capture_output=True, text=True)
@@ -89,7 +115,7 @@ def install_python():
 def install_pip():
     try:
         subprocess.run(['sudo', 'apt', 'update'])
-        subprocess.run(['sudo', 'apt', 'install', 'python3-pip'])
+        subprocess.run(['sudo', 'apt', 'install', 'python-pip'])
         print("Pip was installed successfully.")
     except Exception as e:
         print(f"Pip install failed...")
@@ -98,9 +124,9 @@ def install_pip():
 def install_mysql():
     try:
         subprocess.run(['sudo', 'apt', 'update'])
-        subprocess.run(['sudo', 'apt', 'install', 'mysql-server', 'mysql-client', '-y'])
+        subprocess.run(['sudo', 'apt', 'install', 'mysql-server', '-y'])
         mysql_status = subprocess.run(['systemctl', 'is-active', 'mysql'], capture_output=True)
-        if 'active' not in mysql_status.stdout:
+        if mysql_status.stdout != 'active':
             subprocess.run(['sudo', 'systemctl', 'start', 'mysql'])
         else:
             version_info = subprocess.run(['mysql', '--version'], capture_output=True, text=True)
@@ -145,13 +171,15 @@ def main():
     check_requirements()
     install_environment()
     mysql_user = input("Plese enter your user name for Mysql DB: ")
-    load_data_mysql(mysql_user)
+    check_mysql_permissions(mysql_user)
+    #load_data_mysql()
     mysql_password = input("Plese enter password for Mysql DB: ")
     mysql_host = input("Plese enter host for Mysql DB: ")
     mysql_port = input("Plese enter port for Mysql DB: ")
-    email_host = input("Plese enter email host for Mysql DB: ")
+    email_host_user = input("Plese enter email host for Mysql DB: ")
     email_host_password = input("Plese enter email host password for Mysql DB: ")
-    modify_settings(mysql_user, mysql_password, mysql_host, mysql_port, email_host, email_host_password)
+    modify_settings(mysql_user, mysql_password, mysql_host, mysql_port, email_host_user, email_host_password)
+    run_server()
 
 if __name__ == "__main__":
     main()
