92544ded2fad5063c31349189cbad3e44085f987
Artem Ushenko <you@example.com>
2023-08-25 10:32:14 +0300

    Updated scripting.py

--- scripting.py
+++ scripting.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
 import os
 import subprocess
@@ -14,29 +14,18 @@ def check_mysql_permissions(mysql_user):
     else:
         print(f"Access denied to Mysql for {mysql_user}.")
 
-    pass
 def run_server():
-    subprocess.run(["python simple-django-project/manage.py makemigrations"], shell=True)
-    subprocess.run(["python simple-django-project/manage.py migrate"], shell=True)
-    subprocess.run(["python simple-django-project/manage.py rebuild_index"], shell=True)
-    subprocess.run(["python simple-django-project/manage.py runserver 0:8001"], shell=True)
-    print("Your server is up on port 8001")
-
-#def load_data_mysql():
-    #subprocess.run(["sudo", "mysql_secure_installation"])
+    try:
+        subprocess.run(["python3 simple-django-project/manage.py makemigrations"], shell=True)
+        subprocess.run(["python3 simple-django-project/manage.py migrate"], shell=True)
+        subprocess.run(["python3 simple-django-project/manage.py rebuild_index"], shell=True)
+        subprocess.run(["python3 simple-django-project/manage.py runserver 0:8001"], shell=True)
+        print("Your server is up on port 8001")
+    except Exception as e:
+        print("Something gone wrong. Please use README.md to install all needed tools.")
+        print(str(e))
+        exit()
    
-    #subprocess.run(["mysql -u root -p"], shell=True)
-    #mysql_user = 'bob'
-    #mysql_password = 'P@ssword123'
-    #mysql_queries = [
-    #        f"CREATE USER '{mysql_user}'@'localhost' IDENTIFIED BY '{mysql_password}';",
-    #        f"GRANT ALL PRIVILEGES ON world.* TO '{mysql_user}'@'localhost';",
-    #        f"FLUSH PRIVILEGES;"
-    #        ]
-    
-    #for command in mysql_queries:
-    #    subprocess.run([f'mysql -u root -p -e {command}'], shell=True) 
-    
 def modify_settings(mysql_user, mysql_password, mysql_host, mysql_port, email_host_user, email_host_password):
 
     # Temporarily add the directory containing settings.py to sys.path
@@ -84,66 +73,31 @@ def modify_settings(mysql_user, mysql_password, mysql_host, mysql_port, email_ho
 
 def install_environment():
     try:
-        subprocess.run(["python", "-m", "venv", "new_venv"])
+        subprocess.run(["pip", "install", "virtualenv"])
+        subprocess.run(["python3", "-m", "virtualenv", "envs"])
         git_path = "https://github.com/Manisha-Bayya/simple-django-project.git"
         subprocess.run(["git", "clone", git_path])
         status = subprocess.run(["pip", "install", "-r", "requirements.txt"], capture_output=True, text=True)
         if status.returncode == 0:
             print("Virtual environment installed successfully.")
         else:
-            print(f"Virtual environment not installed. Error {status.stderr}")
+            print(f"Virtual environment not installed. Error {status.stderr}. Please use README.md to install all needed tools.")
+            exit()
     except Exception as e:
-        print("Something gone wrong.")
+        print("Something gone wrong. Please use README.md to install all needed tools.")
         print(str(e))
-
-def install_python():
-    try:
-        subprocess.run(['mkdir', 'temp'])
-        subprocess.run(['cd', 'temp'])
-        subprocess.run(['wget', 'https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz'])
-        subprocess.run(['tar', '-xvf', 'Python-3.7.2.tgz'])
-        subprocess.run(['rm', '-rf', 'Python-3.7.2.tgz'])
-        subprocess.run(['cd', 'Python-3.7.2'])
-        subprocess.run(['./configure'])
-        subprocess.run(['make'])
-        subprocess.run(['sudo', 'make', 'install']) 
-        print("Python 3.7.2 was installed successfully.")
-    except Exception as e:
-        print(f"Python 3.7.2 install failed...")
-        print(str(e))
-   
-def install_pip():
-    try:
-        subprocess.run(['sudo', 'apt', 'update'])
-        subprocess.run(['sudo', 'apt', 'install', 'python-pip'])
-        print("Pip was installed successfully.")
-    except Exception as e:
-        print(f"Pip install failed...")
-        print(str(e))
-
-def install_mysql():
-    try:
-        subprocess.run(['sudo', 'apt', 'update'])
-        subprocess.run(['sudo', 'apt', 'install', 'mysql-server', '-y'])
-        mysql_status = subprocess.run(['systemctl', 'is-active', 'mysql'], capture_output=True)
-        if mysql_status.stdout != 'active':
-            subprocess.run(['sudo', 'systemctl', 'start', 'mysql'])
-        else:
-            version_info = subprocess.run(['mysql', '--version'], capture_output=True, text=True)
-            print(f"{version_info.stdout[:17]} was installed successfully.")
-    except Exception as e:
-        print(f"Mysql 8 install failed.")
-        print(str(e))
-
+        exit()
+  
 def check_requirements():
+    
     #check Python version 
     if sys.version_info.major < 3:
-        print(f"Your Python version {sys.version_info.major} does not meet the requirements.\nPython 3.7.2 will be installed now : ")
-        install_python()
+        print(f"Your Python version {sys.version_info.major} does not meet the requirements. Please use README.md to install all needed tools.")
+        exit()
     if sys.version_info.minor < 7:
-        print(f"Your Python version {sys.version_info.major} + '.' + {sys.version_info.minor} does not meet the requirements.\nPython 3.7.2 will be installed now : ")
-        install_python()
-    if sys.version_info.minor >= 7:
+        print(f"Your Python version {sys.version_info.major} + '.' + {sys.version_info.minor} does not meet the requirements. Please use README.md to install all needed tools.")
+        exit() 
+    if sys.version_info.major == 3 and sys.version_info.minor >= 7:
         version_info = subprocess.run(['python3', '--version'], capture_output=True)
         print(f"{version_info.stdout} already installed and meet the requirements")
 
@@ -152,27 +106,25 @@ def check_requirements():
         version_info = subprocess.run(["pip", "--version"])
         print("Pip is already installed on your system.")
     except:
-        print("Pip was not found on your system. It will be install now: ")
-        install_pip()
-
+        print("Pip was not found on your system. Please use README.md to install all needed tools.")
+        exit()
     #check MySql version
     try:
         version_info = subprocess.run(['mysql', '--version'], capture_output=True, text=True)
         if 'mysql  Ver 8' in version_info.stdout:
             print(f"{version_info.stdout[:17]} already installed." )
         else:
-            print("Mysql 8 will be install now: ")
-            install_mysql()
+            print(f"Your {version_info.stdout[:17]} does not meet the requirements. Please use README.md to install all needed tools.")
+            exit() 
     except:
-        print("Mysql was not found on your system. It will be install now: ")
-        install_mysql()
+        print("Mysql was not found on your system. Please use README.md to install all needed tools.")
+        exit()
 
 def main():
     check_requirements()
     install_environment()
     mysql_user = input("Plese enter your user name for Mysql DB: ")
     check_mysql_permissions(mysql_user)
-    #load_data_mysql()
     mysql_password = input("Plese enter password for Mysql DB: ")
     mysql_host = input("Plese enter host for Mysql DB: ")
     mysql_port = input("Plese enter port for Mysql DB: ")
